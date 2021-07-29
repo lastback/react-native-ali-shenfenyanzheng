8215b48ca0b8ef72b29360cac6a12beffe4fafe0
lastback <nick.kai.lee@qq.com>
2021-07-29 21:06:27 +0800

    修正接口参数调用

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { NativeModules } from 'react-native';
 const { RNAliShenfenyanzheng } = NativeModules;
 
 export async function verifiy(url, certifyId, bizCode) {
-  return await RNAliShenfenyanzheng.verifiy(url, certifyId);
+  return await RNAliShenfenyanzheng.verifiy(url, certifyId, bizCode);
 }
 
 export async function getBizCode() {
