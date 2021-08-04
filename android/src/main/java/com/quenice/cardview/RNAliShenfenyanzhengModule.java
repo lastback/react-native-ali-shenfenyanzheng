d96a45d74b1d49a52e357a60c8fc818698fcac3b
lastback <nick.kai.lee@qq.com>
2021-08-04 13:48:42 +0800

    android sdk接入

--- android/src/main/java/com/quenice/cardview/RNAliShenfenyanzhengModule.java
+++ android/src/main/java/com/quenice/cardview/RNAliShenfenyanzhengModule.java
@@ -1,10 +1,15 @@
 
 package com.quenice.cardview;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alipay.mobile.android.verify.sdk.ServiceFactory;
+import com.alipay.mobile.android.verify.sdk.interfaces.ICallback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.util.Map;
 
 public class RNAliShenfenyanzhengModule extends ReactContextBaseJavaModule {
 
@@ -16,7 +21,41 @@ public class RNAliShenfenyanzhengModule extends ReactContextBaseJavaModule {
   }
 
   @Override
-  public String getName() {
-    return "RNAliShenfenyanzheng";
-  }
+public String getName() {
+  return "RNAliShenfenyanzheng";
+}
+
+private String certifyId;
+private String bizCode;
+@ReactMethod
+public void getBizCode(final Promise promise) {
+  bizCode = ServiceFactory.build().getBizCode(reactContext);
+  promise.resolve(bizCode);
+}
+
+
+
+@ReactMethod
+public void verifiy(String  url, String acertifyId, String abizCode){
+  // 封装认证数据
+  certifyId =acertifyId;
+  bizCode = abizCode;//ServiceFactory.build().getBizCode(reactContext);
+  JSONObject requestInfo = new JSONObject();
+  requestInfo.put("url", url);
+  requestInfo.put("certifyId", certifyId);
+  requestInfo.put("bizCode", bizCode);
+
+
+//     发起认证
+  ServiceFactory.build().startService(reactContext.getCurrentActivity(), requestInfo, new ICallback() {
+    @Override
+    public void onResponse(Map<String, String> response) {
+      String responseCode = response.get("resultStatus");
+      if ("9001".equals(responseCode)) {
+        // 9001需要等待回调/回前台查询认证结果
+      }
+
+    }
+  });
+}
 }
